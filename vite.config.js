5e1d47fecc1f8b44e5ef029bfc7dc2279b40934c
HIMANSHU KUSHWAHA <151002231+HIMANSHU-KUSHWAHA-2004@users.noreply.github.com>
2025-06-30 13:28:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/My_Portfolio/',  // <- your GitHub repo name with slashes
+  base: '/',  // <- your GitHub repo name with slashes
   plugins: [react()],
 });
