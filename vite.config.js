8b49d4eff0e5a7eb9c2bd519d2ad903c65f6e7f7
HIMANSHU-KUSHWAHA-2004 <hk5652990@gmail.com>
2025-08-01 18:11:21 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // <- your GitHub repo name with slashes
+  base: '/My_Portfolio/', // <- your GitHub repo name with slashes
   plugins: [react()],
   server: {
     host: true,
